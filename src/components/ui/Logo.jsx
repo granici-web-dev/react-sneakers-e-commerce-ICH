f6dd891aad96c5eab2cbeee18b1569e42633a804
Serghei_Granici <granici.design@gmail.com>
2026-03-25 18:47:30 +0100

    Logo link added

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
-const StyledLogo = styled.div`
+const StyledLogo = styled.a`
   font-size: 1.4rem;
   font-weight: 700;
   color: #fff;
 `;
 
 function Logo() {
-  return <StyledLogo>sneaker-store</StyledLogo>
+  return <StyledLogo href='/'>sneaker-store</StyledLogo>
 }
 
 
